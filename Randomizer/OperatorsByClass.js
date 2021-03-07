5a026467a0376284960daf46862d28f50fda7f96
OOSad <77910065+OOSad@users.noreply.github.com>
2021-03-07 02:31:50 -0300

    Somehow got debugging on chromium to work

---
+++ Randomizer/OperatorsByClass.js
@@ -0,0 +1,10 @@
+var guardOperators = ["Flint", "Thorns", "Ayerscarpe", "Sideroca", "Cutter", "Bibeak", "Utage", "Blaze", "Broca", "Flamebringer", "Hellagur", "Astesia", "Ch'en", "Swire", "Popukar", "Skadi", "Beehunter", "Midnight", "Savage", "SilverAsh", "Specter", "Lappland", "Indra", "Franka", "Mousse", "Estelle", "Frostleaf", "Matoimaru", "Dobermann", "Melantha", "Castle-3"];
+
+
+document.getElementById('RollATeamButton').onclick = function() 
+{
+
+    console.log("hi");
+    
+}
+

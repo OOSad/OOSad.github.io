7184c87b73bfd9a1be7fe192340a5e2deb0402ea
OOSad <77910065+OOSad@users.noreply.github.com>
2021-03-07 02:56:52 -0300

    Got Roll A Team button to do something on click

--- Randomizer/OperatorsByClass.js
+++ Randomizer/OperatorsByClass.js
@@ -1,10 +1,11 @@
 var guardOperators = ["Flint", "Thorns", "Ayerscarpe", "Sideroca", "Cutter", "Bibeak", "Utage", "Blaze", "Broca", "Flamebringer", "Hellagur", "Astesia", "Ch'en", "Swire", "Popukar", "Skadi", "Beehunter", "Midnight", "Savage", "SilverAsh", "Specter", "Lappland", "Indra", "Franka", "Mousse", "Estelle", "Frostleaf", "Matoimaru", "Dobermann", "Melantha", "Castle-3"];
 
+var rollATeamButton = document.getElementById("RollATeamButton"); 
 
-document.getElementById('RollATeamButton').onclick = function() 
+rollATeamButton.onclick = function()
 {
-
-    console.log("hi");
-    
+    for (var i = 0; i < 12; i++)
+    {
+        console.log(guardOperators[i]);
+    }
 }
-

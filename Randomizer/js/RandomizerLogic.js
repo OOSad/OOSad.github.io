6911bee2542d65d17082b3d18130dd01b8644030
OOSad <77910065+OOSad@users.noreply.github.com>
2021-03-18 08:06:21 -0300

    Changed first dummy checkbox functionality to check all the other user operator checkboxes on page when checked

--- Randomizer/js/RandomizerLogic.js
+++ Randomizer/js/RandomizerLogic.js
@@ -3,6 +3,8 @@
 var completePoolOfOperators = [];
 var completePoolOfOperatorsDefault = ["Folinic", "Ceylon", "Breeze", "Sussurro", "Nightingale", "Shining", "Warfarin", "Silence", "Ptilopsis", "Perfumer", "Gavial", "Myrrh", "Ansel", "Hibiscus", "Lancet-2", "Scene", "Suzuran", "Podenco", "Tsukinogi", "Shamare", "Magallan", "Glaucus", "Angelina", "Sora", "Istina", "Pramanix", "Mayer", "Earthspirit", "Deepcolor", "Orchid", "Aciddrop", "Andreana", "Rosa(Poca)", "W", "Sesa", "GreyThroat", "Ambriel", "May", "Executor", "Vermeil", "Schwarz", "Catapult", "Exusiai", "Firewatch", "Provence", "Meteorite", "Platinum", "Blue Poison", "Shirayuki", "Meteor", "Jessica", "Adnachiel", "Kroos", "Rangers", "Chiave", "Elysium", "Bagpipe", "Reed", "Myrtle", "Grani", "Siege", "Texas", "Zima", "Vigna", "Scavenger", "Courier", "Plume", "Vanilla", "Fang", "Yato", "Jaye", "Weedy", "THRM-EX", "Phantom", "Aak", "Snowsant", "Waai Fu", "Ethan", "FEater", "Manticore", "Cliffheart", "Projekt Red", "Shaw", "Rope", "Gravel", "Eunectes", "Asbestos", "Nian", "Hung", "Bison", "Dur-nar", "Spot", "Saria", "Hoshiguma", "Vulcan", "Croissant", "Liskarm", "Nearl", "Gummy", "Cuora", "Matterhorn", "Beagle", "Cardigan", "Noir Corne", "Flint", "Thorns", "Ayerscarpe", "Sideroca", "Cutter", "Bibeak", "Utage", "Blaze", "Broca", "Flamebringer", "Hellagur", "Astesia", "Ch'en", "Swire", "Popukar", "Skadi", "Beehunter", "Midnight", "Savage", "SilverAsh", "Specter", "Lappland", "Indra", "Franka", "Mousse", "Estelle", "Frostleaf", "Matoimaru", "Dobermann", "Melantha", "Castle-3", "Tomimi", "Beeswax", "Click", "Leonhardt", "Absinthe", "Ceobe", "Leizi", "Mostima", "Greyy", "Nightmare", "Eyjafjalla", "Ifrit", "Skyfire", "Amiya", "Gitano", "Haze", "Steward", "Lava", "12F", "Durin"];
 
+var userPoolOfOperators = [];
+
 var rolledPoolOfOperators = [];
 var userOperatorCheckboxes = [];
 
@@ -86,6 +88,7 @@ for (var i = 0; i < completePoolOfOperatorsDefault.length; i++)
 }
 
 var userOperatorCheckboxes = document.getElementsByClassName("UserOperatorCheckbox");
+userOperatorCheckboxes[0].setAttribute("onchange", "CheckAllTheUserOperatorCheckboxes(this)");
 
 
 if (JSON.parse(localStorage.getItem("UserOps") == null))
@@ -633,6 +636,25 @@ function FilterOutIndividualPoolOfMaps(stagesToFilter)
             }
 
             else {}
+        }
+    }
+}
+
+function CheckAllTheUserOperatorCheckboxes(callerCheckbox)
+{
+    if (callerCheckbox.checked)
+    {
+        for (var i = 0; i < userOperatorCheckboxes.length; i++)
+        {
+            userOperatorCheckboxes[i].setAttribute("checked", "true");
+        }
+    }
+
+    else if (!callerCheckbox.checked)
+    {
+        for (var i = 0; i < userOperatorCheckboxes.length; i++)
+        {
+            userOperatorCheckboxes[i].setAttribute("checked", "false");
         }
     }
 }
